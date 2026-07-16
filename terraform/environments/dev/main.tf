f70d05fe5c142651c784259eb7afcc82bda0193b
Ganesh R <ganeshrjune@gmail.com>
2026-07-16 19:46:41 +1000

    Add reusable Azure networking module

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,20 @@
+module "networking" {
+  source = "../../modules/networking"
+
+  resource_group_name = "rg-aep-dev-001"
+  location            = "Australia East"
+
+  vnet_name     = "vnet-aep-dev-001"
+  address_space = ["10.10.0.0/16"]
+
+  web_subnet_prefix        = ["10.10.1.0/24"]
+  app_subnet_prefix        = ["10.10.2.0/24"]
+  management_subnet_prefix = ["10.10.3.0/24"]
+
+  tags = {
+    Environment = "Development"
+    Project     = "Azure Enterprise Platform"
+    ManagedBy   = "Terraform"
+    Owner       = "Ganesh"
+  }
+}
